02fc08ae14c007d2f90ab0f6990733483f8b2125
Tengu-64 <igor_kutsenko64@mail.ru>
2023-08-23 23:11:27 +0300

    added index.html, configured view engine

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 const express = require('express')
+const path = require('path')
 require('dotenv').config()
 
 const app = express()
 const PORT = process.env.PORT
+
+app.set('view engine', 'ejs')
+app.use(express.static('assets'))
+app.use(express.static('images'))
+
 
 app.use((req, res, next) => {
     console.log(`localhost:${PORT}${req.path}`)
     next()
 })
 
+
+
 app.get('/', (req, res) => {
-    res.send('<h1>Index</h1>')
+    res.render('index')
 })
 
 app.listen(PORT, () => console.log('server is running!'))
