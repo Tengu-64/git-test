5ba2ef81e92aa3b3dca0459de0c33067e0b76b50
Tengu-64 <igor_kutsenko64@mail.ru>
2023-08-24 00:21:59 +0300

    the template is divided into components, a biography section is added, an error handler is added

--- index.js
+++ index.js
@@ -16,9 +16,20 @@ app.use((req, res, next) => {
 })
 
 
-
 app.get('/', (req, res) => {
-    res.render('index')
+    const data_home = { 'title': 'манга Дзюндзи Ито' }
+    res.render('index', data_home)
+})
+
+app.get('/biography', (req, res) => {
+    const data_biography = {'title': 'биография дзюндзи ито'}
+    res.render('biography', data_biography)
 })
 
 app.listen(PORT, () => console.log('server is running!'))
+
+
+app.use((req, res,) => {
+    const data_err = { 'title': 'ERROR404' }
+    res.status(404).render('error', data_err)
+})
