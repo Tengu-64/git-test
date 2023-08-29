bb2be5c5d8a715e9eb75acedd0040330d1568ea2
Tengu-64 <igor_kutsenko64@mail.ru>
2023-08-29 18:22:13 +0300

    Added the main router, post request for the registration and authorization page

--- index.js
+++ index.js
@@ -1,33 +1,20 @@
 const express = require('express')
-const path = require('path')
 require('dotenv').config()
-
 const app = express()
 const PORT = process.env.PORT
 
 app.set('view engine', 'ejs')
 app.use(express.static('assets'))
 app.use(express.static('images'))
+app.use(express.urlencoded({ extended: false }))
 
 
-app.use((req, res, next) => {
-    console.log(`localhost:${PORT}${req.path}`)
-    next()
-})
+const indexRouter = require('./routes/index')
+app.use('/', indexRouter)
 
 
-app.get('/', (req, res) => {
-    const data_home = { 'title': 'манга Дзюндзи Ито' }
-    res.render('index', data_home)
-})
-
-app.get('/biography', (req, res) => {
-    const data_biography = {'title': 'биография дзюндзи ито'}
-    res.render('biography', data_biography)
-})
 
 app.listen(PORT, () => console.log('server is running!'))
-
 
 app.use((req, res,) => {
     const data_err = { 'title': 'ERROR404' }
